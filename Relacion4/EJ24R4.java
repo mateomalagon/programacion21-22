3041a637e8b47c57efbd98e316e9eaf3c7757a03
mateomalagon <91953438+mateomalagon@users.noreply.github.com>
2021-10-18 18:53:06 +0200

    Add files via upload

--- Relacion4/EJ24R4.java
+++ Relacion4/EJ24R4.java
@@ -1,7 +1,6 @@
 public class EJ24R4 {
 	
 	public static void main (String[] args) {
-  
 /*Escribe un programa que genere la nómina (bien desglosada) de un empleado
 según las siguientes condiciones:
 • Se pregunta el cargo del empleado (1 - Prog. junior, 2 - Prog. senior, 3 - Jefe
@@ -15,46 +14,47 @@ concepto de dietas. Al sueldo neto hay que restarle el IRPF, que será de
 un 25% en caso de estar soltero y un 20% en caso de estar casado.*/
 
 
-  System.out.println("1 - Programador junior\n 2 - Prog. senior\n 3 - Jefe de proyecto");
-  System.out.println("¿Introduce el cargo del empleado (1 - 3): ");
+  System.out.println("1 - Programador junior\n2 - Prog. senior\n3 - Jefe de proyecto");
+  System.out.printf("¿Introduce el cargo del empleado (1 - 3): ");
     int cargo = Integer.parseInt(System.console().readLine());
-  System.out.println("¿Cuántos días ha estado de viaje visitando clientes? ");
+  System.out.printf("¿Cuántos días ha estado de viaje visitando clientes? ");
     int dieta = Integer.parseInt(System.console().readLine());
-  System.out.println("Introduce su estado civil (1 - Soltero, 2 - Casado):");
+  System.out.printf("Introduce su estado civil (1 - Soltero, 2 - Casado):");
     int civil = Integer.parseInt(System.console().readLine());
-  
-  
-  double irpf=0, base=0;
+    
+    
+    double irpf=0, base=0; 
+    
   if (cargo==1) {
-     base = 950.00;
+    base = 950.00;
 	}
   if (cargo==2) {
-     base = 1200.00;
+    base = 1200.00;
 	}
   if (cargo==3) {
-     base = 1600.00;
+    base = 1600.00;
 	}
-  
   if (civil==1) {
-     irpf = 0.25;
-     String IRPF;
-     IRPF = "(25%)";
-}
+    irpf = 0.25;
+  }
   if (civil==2) {
-     irpf = 0.2;
-     String IRPF;
-     IRPF = "(20%)";
-}
-  double bruto, dietaf;
+    irpf = 0.2;
+  }
+  double bruto, dietaf, retencion;
   dietaf = dieta*30;
   bruto = dietaf+base;
+  retencion = bruto*irpf;
+  
   System.out.println("Sueldo base:      "+base);
-  System.out.println("Dietas "+"("+dieta+" viajes:      "+dietaf);
+  System.out.println("Dietas "+"("+dieta+" viajes): "+dietaf);
   System.out.println("------------------------");
   System.out.println("Sueldo bruto:     "+(bruto));
+  System.out.println("Retención IRPF:    "+retencion);
   
-  System.out.println("Retención IRPF "+IRPF+":      "+(bruto*irpf));
   System.out.println("------------------------");
   System.out.println("Sueldo neto:      "+(bruto-(bruto*irpf)));
   }
-}
+  }
+
+
+
