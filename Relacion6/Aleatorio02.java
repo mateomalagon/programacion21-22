59dd9ed32a7ab286569059cc26e39a807ccb8b0a
mateomalagon <91953438+mateomalagon@users.noreply.github.com>
2021-11-10 13:00:08 +0100

    Add files via upload

---
+++ Relacion6/Aleatorio02.java
@@ -0,0 +1,12 @@
+public class Aleatorio02 {
+  public static void main(String[] args) {
+
+  System.out.println("20 números aleatorios entre 0 y 10");
+  System.out.println(" sin llegar a 10 (con decimales):");
+
+  for (int i = 1; i <= 20; i++) {
+    System.out.println( Math.random()*10 + " ");
+  }
+
+  }
+}
---
+++ Relacion6/Aleatorio03.java
@@ -0,0 +1,12 @@
+public class Aleatorio03 {
+  public static void main(String[] args) {
+  
+  System.out.println("20 números aleatorios entre 0 y 9 (sin decimales):");
+  
+  for (int i = 1; i <= 20; i++) {
+    System.out.print((int)(Math.random()*10) + " ");
+  }
+  
+  System.out.println();
+  }
+}
