c687d64cde96b90f80ea7c213cd82448d08058c4
mateomalagon <91953438+mateomalagon@users.noreply.github.com>
2021-11-16 12:03:38 +0100

    Update EJ14R6.java

--- Relacion6/EJ14R6.java
+++ Relacion6/EJ14R6.java
@@ -1,9 +1,10 @@
 public class EJ14R6 {
   public static void main(String[] args) {
-/*Escribe un programa que piense un número al azar entre 0 y 100. El usuario debe adivinarlo y 
-tiene para ello 5 oportunidades. Después de cada intento fallido,
-el programa dirá cuántas oportunidades quedan y si el número introducido es
-menor o mayor que el número secreto.*/
+/*Realiza un programa que haga justo lo contrario a lo que hace el ejercicio 6. El
+programa intentará adivinar el número que estás pensando - un número entre 0
+y 100 - teniendo para ello 5 oportunidades. En cada intento fallido, el programa
+debe preguntar si el número que estás pensando es mayor o menor que el que
+te acaba de decir*/
 
   int oportunidades = 5;
   int n;
